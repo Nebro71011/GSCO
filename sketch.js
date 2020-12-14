847670e23f43ed6920814fbd4fc376623f13f9dd
Nebro71011 <75066951+Nebro71011@users.noreply.github.com>
2020-12-14 08:57:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ function setup() {
   createCanvas(1600,400);
   
   car=createSprite(50, 200, 50, 50);
+   car.velocityX=speed;
   wall=createSprite(1500,200,60,height/2);
 
   speed=random(55,90);
@@ -15,7 +16,7 @@ function setup() {
 function draw() {
   background(255,255,255);  
 
-  car.velocityX=speed;
+ 
  
   if (car.x-wall.x<car.width/2+wall.width/2){
    car.velocityX=0;
